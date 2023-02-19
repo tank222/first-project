2678c96ee98e7ef24ad889b69015c7d0c7496793
tank222 <117488701+tank222@users.noreply.github.com>
2023-02-19 22:50:56 +0300

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+     int a,b;
+     printf(" a = ");
+     scanf("%i" ,&a);
+     printf(" b = ");
+     scanf("%i" ,&b);
+    if(a>b)
+{
+    printf("A bolshe B");
+}
+ else
+{
+     if(a==b)
+     {
+      printf("A=B");
+     }
+     else
+     {
+          printf("B bolshe");
+     }
+ }
+      }
