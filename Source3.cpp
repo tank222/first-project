85a5491c1fca57b616e1c6c0c32012ddd8e925fd
tank222 <117488701+tank222@users.noreply.github.com>
2023-02-21 19:29:07 +0300

    Add files via upload

---
+++ Source3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    
+ int pin1, pin2;
+ printf("pin1=");
+ scanf("%i",&pin1);
+ printf("pin2=");
+ scanf("%i",&pin2);
+ if(((pin1==111) &&(pin2==222)) ||((pin1==111) &&(pin2==222))) 
+ {
+    printf("Ok");
+ }  
+ else
+ {
+    printf("Wrong input");
+ }
+      }
