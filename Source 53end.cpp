79d8508fb542e6664cb41f23b984898f308127d8
tank222 <117488701+tank222@users.noreply.github.com>
2023-03-15 22:39:48 +0300

    Add files via upload

---
+++ Source 53end.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main()
+{
+ int mas[100][100];
+ int n,m;
+ printf("n="); 
+ scanf("%d",&n);
+ 
+  printf("m="); 
+  scanf("%d",&m);
+  
+  srand(time(NULL));
+  
+ for(int j=0;j<n;j++)
+  {
+  for(int i=0;i<m;i++)
+   {
+     mas[j][i]=rand()%7+(-2);
+   }
+  }
+  for(int j=0;j<n;j++)
+  {
+  for(int i=0;i<m;i++)
+   {
+     printf("%7i",mas[j][i]);
+   }
+   printf("\n\n");
+  }
+   }
